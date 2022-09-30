915504a1622ea94278991e349697b1e9a88622aa
AntonSvirid <adsvirid@tut.by>
2022-09-30 10:46:10 +0300

    добавлены алгоритмы посика первого,последнего совпадения искомого значения и счётчика повторений элемента в массиве

--- src/by/itstep/antonsvirid/model/ArrayWorker.java
+++ src/by/itstep/antonsvirid/model/ArrayWorker.java
@@ -10,4 +10,34 @@ public class ArrayWorker {
         }
         return false;
     }
+
+    public static int findFirstIndexValue(int[] array, int value){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int findLastIndexValue(int[] array, int value){
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int countValue(int[] array, int value) {
+        int count = 0;
+
+        for (int element : array) {
+            if (element == value) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
