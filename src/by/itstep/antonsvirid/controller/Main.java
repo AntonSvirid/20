037d783b6769c04d90e10a7059900635f8cf4e79
AntonSvirid <adsvirid@tut.by>
2022-09-30 11:54:08 +0300

    add check count for binnary & linean (is it work&) search

--- src/by/itstep/antonsvirid/controller/Main.java
+++ src/by/itstep/antonsvirid/controller/Main.java
@@ -1,13 +1,26 @@
 package by.itstep.antonsvirid.controller;
 
+import by.itstep.antonsvirid.model.ArrayWorker;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};  // объявление и инициализация массива
 
+        int size = 1_000_000;
+        int array[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = i;
+        }
+
+        ArrayWorker.checkValue(array, 2_000_000);
+        ArrayWorker.binarySearch(array, 2_000_000);
+    }
+
+    //       int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};  // объявление и инициализация массива
 
 
 //        for (int item : array) {    // - foreach - перебор элементов массива, присваивание элементам
-                                        //   переменной item
+    //   переменной item
 //            item *= 2;                // что нужно сдлеать с очередным элементом
 //        }
 //
@@ -15,6 +28,6 @@ public class Main {
 //            System.out.println(item + " ");
 //        }
 
-       // System.out.println(array); - не работает, массив - ссылочная переменная
-    }
+    // System.out.println(array); - не работает, массив - ссылочная переменная
 }
+
--- src/by/itstep/antonsvirid/model/ArrayWorker.java
+++ src/by/itstep/antonsvirid/model/ArrayWorker.java
@@ -3,15 +3,25 @@ package by.itstep.antonsvirid.model;
 public class ArrayWorker {
     public static boolean checkValue(int[] array, int value) {
 
-        for (int item : array) {        // алгоритм поиска значения в массиве (есть/нет?)
+        int count = 0;  //debug - доразобрать почемуне работает!!!
+
+        for (int item : array) {
+
+        return true;
+        }
+
+
+        for (int item : array) {
+            count++;    // алгоритм поиска значения в массиве (есть/нет?)
             if (item == value) {
                 return true;
             }
         }
+        System.out.println("count = " + count);
         return false;
     }
 
-    public static int findFirstIndexValue(int[] array, int value){
+    public static int findFirstIndexValue(int[] array, int value) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == value) {
                 return i;
@@ -20,7 +30,7 @@ public class ArrayWorker {
         return -1;
     }
 
-    public static int findLastIndexValue(int[] array, int value){
+    public static int findLastIndexValue(int[] array, int value) {
         for (int i = array.length - 1; i >= 0; i--) {
             if (array[i] == value) {
                 return i;
@@ -39,5 +49,28 @@ public class ArrayWorker {
         }
 
         return count;
+    }
+
+    public static boolean binarySearch(int[] array, int value) {
+        int count = 0;  //debug
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            count++;
+
+            int middle = (right + left) / 2;
+
+            if (value == array[middle]) {
+                System.out.println("count = " + count);
+                return true;
+            } else if (value > array[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        System.out.println("count = " + count);
+        return false;
     }
 }
